ae62b718d560ab53b65d80f697f4aba05400e70a
Kacper Grzywacz <kacpergrzywacz2@gmail.com>
2025-11-04 17:59:25 +0100

    add: navigation for FightScreen in App.jsx

--- App.jsx
+++ App.jsx
@@ -2,6 +2,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import StartScreen from './components/screens/StartScreen';
+import FightScreen from './components/screens/FightScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -10,6 +11,7 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator initialRouteName='Start'>
         <Stack.Screen name='Start' component={StartScreen} />
+        <Stack.Screen name='Battle' component={FightScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
