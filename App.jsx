6ff731ae436c05f2f158c14dbe0df5d9ca3cd855
Kacper Grzywacz <kacpergrzywacz2@gmail.com>
2025-11-04 18:58:10 +0100

    add: ScreenThree component with styles

--- App.jsx
+++ App.jsx
@@ -6,6 +6,7 @@ import FightScreen from './components/screens/FightScreen';
 import FinalScreen from './components/screens/FinalScreen';
 import ScreenOne from './components/screens/ScreenOne';
 import ScreenTwo from './components/screens/ScreenTwo';
+import ScreenThree from './components/screens/ScreenThree';
 
 const Stack = createNativeStackNavigator();
 
@@ -13,7 +14,7 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName='Start'>
-        <Stack.Screen name='Start' component={ScreenTwo} />
+        <Stack.Screen name='Start' component={ScreenThree} />
         <Stack.Screen name='Battle' component={FightScreen} />
         <Stack.Screen name='The End' component={FinalScreen} />
       </Stack.Navigator>
